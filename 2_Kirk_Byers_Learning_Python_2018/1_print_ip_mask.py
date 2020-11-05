e6d0a6ebab8a2bff061034e3d210cac7a90047b6
pithei <rwx2dev@gmail.com>
2020-11-05 22:21:31 +0200

    byers 1

--- 2_Kirk_Byers_Learning_Python_2018/1_print_ip_mask.py
+++ 2_Kirk_Byers_Learning_Python_2018/1_print_ip_mask.py
@@ -7,3 +7,16 @@ If you are using either Linux or MacOS make your program executable
 by adding a shebang line and by changing the files permissions (i.e. chmod 755 exercise1.py).
 
 """
+
+from __future__ import print_function
+
+ip_addr1 = "10.0.0.1"
+ip_addr2 = "10.0.0.2"
+ip_addr3 = "10.0.0.3"
+
+print(ip_addr1, ip_addr2, ip_addr3 )
+
+""" try:
+    ip_addr = raw_input("Enter the IP, py2: ")
+except NameError:
+    ip_addr = input("Enter the IP, py3: ") """
