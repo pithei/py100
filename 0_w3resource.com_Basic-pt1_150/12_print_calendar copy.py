2607b6d0d4b4b5a3d003e9bad87fcfd82057cc6f
pithei <rwx2dev@gmail.com>
2020-12-30 23:11:17 +0200

    basics 13, print string

---
+++ 0_w3resource.com_Basic-pt1_150/12_print_calendar copy.py
@@ -0,0 +1,22 @@
+"""
+13. Write a Python program to print the following here document. Go to the editor
+Sample string :
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example
+
+"""
+
+print("""
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example
+""")
+
+
+"""
+Solution, https://www.w3resource.com/python-exercises/python-basic-exercise-13.php
+
+"""
