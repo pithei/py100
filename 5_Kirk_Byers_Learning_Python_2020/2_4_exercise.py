1c85d8c93ce00e7bdd58018a1445fd1852d0186d
pithei <rwx2dev@gmail.com>
2021-01-01 14:22:32 +0200

    byers 2.4 pycodestyle done

--- 5_Kirk_Byers_Learning_Python_2020/2_4_exercise.py
+++ 5_Kirk_Byers_Learning_Python_2020/2_4_exercise.py
@@ -1,32 +1,48 @@
 """
-4. Read in the "show_ip_int_brief.txt" file into your program using the .readlines() method.
+4. Read in the "show_ip_int_brief.txt" file into your program using
+the .readlines() method.
 
-Obtain the list entry associated with the FastEthernet4 interface. 
-You can just hard-code the index at this point since we haven't covered for-loops or regular expressions. 
-Use the string .split() method to obtain both the IP address and the corresponding interface associated with the IP.
+Obtain the list entry associated with the FastEthernet4 interface.
+You can just hard-code the index at this point since we haven't covered
+for-loops or regular expressions.
+Use the string .split() method to obtain both the IP address and
+the corresponding interface associated with the IP.
 
 Create a two element tuple from the result (intf_name, ip_address).
 
 Print that tuple to the screen.
 
-Use pycodestyle on this script. Get the warnings/errors to zero. You might need to 'pip install pycodestyle' on your computer (you should be able to type this from the shell prompt). Alternatively, you can type 'python -m pip install pycodestyle'.
+Use pycodestyle on this script.
+Get the warnings/errors to zero.
+You might need to 'pip install pycodestyle' on your computer
+(you should be able to type this from the shell prompt).
+Alternatively, you can type 'python -m pip install pycodestyle'.
 
 """
-from pprint import pprint
 
 with open("show_ip_int_brief.txt") as f:
     data = f.readlines()
 
-line = data[5].split()
-print(line)
+fields = data[5].split()
 
-intf_name = line[0]
-ip_address = line[1]
+intf_name = fields[0]
+ip_address = fields[1]
 
 interface = (intf_name, ip_address)
 print(interface)
-#pprint(data)
+"""
+Solution, https://github.com/ktbyers/pynet/blob/master/learning_python/lesson2/exercise4.py
 
-#for line in data:
-#    if "FastEthernet4" in line:
-#        print(line)
+from __future__ import print_function, unicode_literals
+
+with open("show_ip_int_brief.txt") as f:
+    show_ip_int_brief = f.readlines()
+
+fa4_ip = show_ip_int_brief[5].strip()
+fields = fa4_ip.split()
+intf = fields[0]
+ip_address = fields[1]
+
+my_results = (intf, ip_address)
+print(my_results)
+"""
