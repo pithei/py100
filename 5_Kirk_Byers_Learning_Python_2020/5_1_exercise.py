c72c17534247bf8a2170defca5349e265cb7b174
pithei <rwx2dev@gmail.com>
2021-01-06 13:25:44 +0200

    byers week 5 materials

---
+++ 5_Kirk_Byers_Learning_Python_2020/5_1_exercise.py
@@ -0,0 +1,17 @@
+"""
+1a. Create an ssh_conn function. This function should have three parameters: ip_addr, username, and password. The function should print out each of these three variables and clearly indicate which variable it is printing out.
+
+Call this ssh_conn function using entirely positional arguments.
+
+Call this ssh_conn function using entirely named arguments.
+
+Call this ssh_conn function using a mix of positional and named arguments.
+
+
+1b. Expand on the ssh_conn function from exercise1 except add a fourth parameter 'device_type' with a default value of 'cisco_ios'. Print all four of the function variables out as part of the function's execution.
+
+Call the 'ssh_conn2' function both with and without specifying the device_type
+
+Create a dictionary that maps to the function's parameters. Call this ssh_conn2 function using the **kwargs technique.
+
+"""
