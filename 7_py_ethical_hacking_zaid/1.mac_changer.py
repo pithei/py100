1d264b5f12a9afeb1b4ed8af3ba704fb07b5ba49
pithei <rwx2dev@gmail.com>
2021-01-17 01:27:54 +0200

    mac changer vars input

--- 7_py_ethical_hacking_zaid/1.mac_changer.py
+++ 7_py_ethical_hacking_zaid/1.mac_changer.py
@@ -17,6 +17,10 @@ import subprocess
 
 interface = "wlan0"
 new_mac = "00:11:22:33:44:66"
+
+new_ip = input("New IP> ")
+new_mac = input("New mac> ")
+print("IP {} and MAC {}".format(new_ip, new_mac))
 # subprocess.call("COMMAND", shell = Ture)
 
 #subprocess.call("ipconfig /all", shell=True)
