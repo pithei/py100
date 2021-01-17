4c607bfde389dc2fbbdd707947c04357a92f5f28
pithei <rwx2dev@gmail.com>
2021-01-17 22:21:34 +0200

    zed, regex + func

--- 7_py_ethical_hacking_zaid/1.3_mac_changer_read_sys_output.py
+++ 7_py_ethical_hacking_zaid/1.3_mac_changer_read_sys_output.py
@@ -1,36 +1,12 @@
 '''
 2021.01.17
-
-getmac /v /FO csv
-
-\w\w:\w\w:\w\w:\w\w:\w\w:\w\w:
+windows getmac command, getmac /v /FO csv
+regex pattern for mac match, \w\w:\w\w:\w\w:\w\w:\w\w:\w\w:
 Algorithm:
     execute and read ifconfig
     read the mac address from output
     check if mac in ifconfig is what the user requested
     print appropriate message
-
-
-data from iproute output
-if_data = "root@vmdev:/home/user# ip a
-1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
-    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
-    inet 127.0.0.1/8 scope host lo
-       valid_lft forever preferred_lft forever
-    inet6 ::1/128 scope host
-       valid_lft forever preferred_lft forever
-2: enp0s3: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP group default qlen 1000
-    link/ether 08:00:27:83:a1:c2 brd ff:ff:ff:ff:ff:ff
-    inet 192.168.88.10/24 brd 192.168.88.255 scope global dynamic noprefixroute enp0s3
-       valid_lft 433sec preferred_lft 433sec
-    inet6 fe80::6295:6731:d9ef:8967/64 scope link noprefixroute
-       valid_lft forever preferred_lft forever
-3: docker0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc noqueue state DOWN group default
-    link/ether 02:42:c7:60:7c:7f brd ff:ff:ff:ff:ff:ff
-    inet 172.17.0.1/16 brd 172.17.255.255 scope global docker0
-       valid_lft forever preferred_lft forever
-root@vmdev:/home/user#"
-""
 '''
 
 import subprocess
@@ -59,39 +35,31 @@ def change_mac(interface, new_mac):
     #subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     #subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+    # search for mac addresses in interface details ( data taken from linux vm )
+    # save ouptut from system command to a variable. PReferably, it should get mac from interface command, ip a show dev {interface}
+    mac_details = subprocess.check_output(["getmac", "-v"])
+    mac_details = mac_details.decode("UTF-8")
+    mac_addresses = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", interface)
+    #print(repr(nic_details))
+    if mac_addresses:
+        return mac_addresses.group(0)
+    else:
+        pass
+        #print("No macs found in input")
+
 argument_list = get_arguments()
 
 
-nic_details = """root@vmdev:/home/user# ip a
-1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
-    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
-    inet 127.0.0.1/8 scope host lo
-       valid_lft forever preferred_lft forever
-    inet6 ::1/128 scope host
-       valid_lft forever preferred_lft forever
+interface = """root@vmdev:/home/user# ip a show dev enp0s3
 2: enp0s3: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP group default qlen 1000
     link/ether 08:00:27:83:a1:c2 brd ff:ff:ff:ff:ff:ff
     inet 192.168.88.10/24 brd 192.168.88.255 scope global dynamic noprefixroute enp0s3
-       valid_lft 433sec preferred_lft 433sec
+       valid_lft 396sec preferred_lft 396sec
     inet6 fe80::6295:6731:d9ef:8967/64 scope link noprefixroute
        valid_lft forever preferred_lft forever
-3: docker0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc noqueue state DOWN group default
-    link/ether 02:42:c7:60:7c:7f brd ff:ff:ff:ff:ff:ff
-    inet 172.17.0.1/16 brd 172.17.255.255 scope global docker0
-       valid_lft forever preferred_lft forever
-root@vmdev:/home/user#"""
+root@vmdev:/home/user#
+"""
 
-# save ouptu from system command to a variable
-mac_details = subprocess.check_output(["getmac", "-v"])
-mac_details = mac_details.decode("UTF-8")
-
-# search for mac addresses in interface details ( data taken from linux vm )
-
-mac_addresses = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", nic_details)
-print(repr(nic_details))
-if mac_addresses:
-    #print("Some Data found")
-    print(mac_addresses.group(0))
-else:
-    print("No macs found in input")
-
+current_mac = get_current_mac(interface)
+print("Current mac", current_mac)
