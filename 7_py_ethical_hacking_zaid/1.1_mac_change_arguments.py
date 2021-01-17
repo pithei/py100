f9c1272261c90220dcf81056693f588855a696ab
pithei <rwx2dev@gmail.com>
2021-01-17 15:01:03 +0200

    mac changer arguments

---
+++ 7_py_ethical_hacking_zaid/1.1_mac_change_arguments.py
@@ -0,0 +1,18 @@
+'''
+2021.01.17
+parse and validate input arguments from user
+optparse module will be used
+'''
+
+import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Defined interface for mac addr change")
+parser.add_option("-m", "--mac", dest="new_mac", help="New mac address")
+
+data = parser.parse_args()
+print(data[0].interface)
+print(data[0].new_mac)
+
