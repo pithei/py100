f795b0c307e66db6f20ec69e2e87923df7cebcbe
pithei <rwx2dev@gmail.com>
2021-01-17 15:31:21 +0200

    zed, mac changer, conditionals

--- 7_py_ethical_hacking_zaid/1.2_mac_changer_functions.py
+++ 7_py_ethical_hacking_zaid/1.2_mac_changer_functions.py
@@ -11,6 +11,13 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Defined interface for mac addr change")
     parser.add_option("-m", "--mac", dest="new_mac", help="New mac address")
+    data = parser.parse_args()
+    if not data[0].interface:
+        # code to handle error
+        parser.error("[-] Please specify interface name, use --help or -h for more information.")
+    elif not data[0].new_mac:
+        # code to handle error
+        parser.error("[-] Please specify new mac, use --help or -h for more information.")
     return parser.parse_args()
 
 def change_mac(interface, new_mac):
