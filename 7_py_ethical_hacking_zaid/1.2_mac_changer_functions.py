c1672af7726df0d69fec924feef070f4c8234f44
pithei <rwx2dev@gmail.com>
2021-01-17 15:21:16 +0200

    mac changer, reorganise

--- 7_py_ethical_hacking_zaid/1.2_mac_changer_functions.py
+++ 7_py_ethical_hacking_zaid/1.2_mac_changer_functions.py
@@ -7,6 +7,12 @@ optparse module will be used
 import subprocess
 import optparse
 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Defined interface for mac addr change")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New mac address")
+    return parser.parse_args()
+
 def change_mac(interface, new_mac):
     print(interface)
     print(new_mac)
@@ -14,11 +20,5 @@ def change_mac(interface, new_mac):
     #subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     #subprocess.call(["ifconfig", interface, "up"])
 
-parser = optparse.OptionParser()
-
-parser.add_option("-i", "--interface", dest="interface", help="Defined interface for mac addr change")
-parser.add_option("-m", "--mac", dest="new_mac", help="New mac address")
-
-data = parser.parse_args()
-
+data = get_arguments()
 change_mac(data[0].interface, data[0].new_mac)
